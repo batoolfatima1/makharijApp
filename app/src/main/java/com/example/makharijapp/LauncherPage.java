ea3717be14e962e0aef861a5a8411e766c22913e
batool fatima <batool12547@gmail.com>
2022-05-21 17:02:37 +0500

    third commit

---
+++ app/src/main/java/com/example/makharijapp/LauncherPage.java
@@ -0,0 +1,14 @@
+package com.example.makharijapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LauncherPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher_page);
+    }
+}
