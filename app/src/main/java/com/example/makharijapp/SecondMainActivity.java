a869e7f2b61673d76aae14372995e6f3487e2025
batool fatima <batool12547@gmail.com>
2022-05-23 10:58:11 +0500

    seventh commit

--- app/src/main/java/com/example/makharijapp/SecondMainActivity.java
+++ app/src/main/java/com/example/makharijapp/SecondMainActivity.java
@@ -5,39 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondMainActivity extends AppCompatActivity implements View.OnClickListener
 {
-    String[] array = {"أ ة ","ع ح","غ خ","ق ","ك ","ج ش ى","ض ","ل  ","ن ","ر ","ط د ت","ظ  ذ  ث","ص ز س","م ن","ف","ب م و ","باَ بوُ بىِ"};
-
-    Button btn = (Button)findViewById(R.id.button2);
-    int index=99;
-    String item;
+    String[] array ={"أ ة","ع ح","غ خ","ق ","ك ","ج ش ى","ض ","ل  ","ن ","ر ","ط د ت","ظ  ذ  ث","ص ز س","م ن","ف","ب م و ","باَ بوُ بىِ"};
+    Button btn,back,next;
+    int index;
+    String item, answer;
+    TextView ans;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_main);
+        btn = (Button)findViewById(R.id.confirm);
+        back =(Button)findViewById(R.id.back);
+        next = findViewById(R.id.next);
+        ans = findViewById(R.id.ans);
 
-
-        btn.setText(array[0]);
-        Button beck = (Button) findViewById(R.id.back);
-        beck.setEnabled(false);
     }
-
-
     @Override
     public void onClick(View v) {
 
         switch(v.getId())
         {
             case R.id.next:
+                back.setEnabled(true);
                 item  = btn.getText().toString();
                 for (int i = 0; i < array.length; i++)
                 {
                     if (item.equals(array[i]))
                         index= i;
                 }
-                btn.setText(array[index+1]);
+                if(index == array.length-1)
+                {
+                    next.setEnabled(false);
+                }
+                else
+                {
+                    item = array[index+1];
+                    btn.setText(array[index+1]);
+                }
+
                 break;
             case R.id.back:
                 item  = btn.getText().toString();
@@ -47,9 +56,51 @@ public class SecondMainActivity extends AppCompatActivity implements View.OnClic
                     if (item.equals(array[i]))
                         index= i;
                 }
-                btn.setText(array[index-1]);
+                if(index == 0)
+                {
+                    back.setEnabled(false);
+                }
+                else
+                {
+                    item = array[index-1];
+                    btn.setText(array[index-1]);
+                }
+
                 break;
+            case R.id.confirm:
+                item  = btn.getText().toString();
+                if(item.equals(array[15]))
+                {
+                    answer = "CORRECT";
+                    ans.setText(answer);
+                }
+                else
+                {
+                    answer = "INCORRECT";
+                    ans.setText(answer);
+                }
+                next.setClickable(false);
+                back.setClickable(false);
+                btn.setClickable(false);
         }
     }
+    @Override
 
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+
+        super.onSaveInstanceState(savedInstanceState);
+
+        savedInstanceState.putString("btnValue",btn.getText().toString());
+        savedInstanceState.putString("answer",ans.getText().toString());
+    }
+
+    @Override
+
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        item = savedInstanceState.getString("btnValue");
+        answer = savedInstanceState.getString("answer");
+        ans.setText(answer);
+        btn.setText(item);
+    }
 }
